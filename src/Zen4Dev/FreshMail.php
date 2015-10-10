96f53387f54f2f0e492a24ffa191934974036ecd
Dawid Holewa <biuro@zen4dev.pl>
2015-10-10 16:17:49 +0200

    Update.

--- src/Zen4Dev/FreshMail.php
+++ src/Zen4Dev/FreshMail.php
@@ -4,7 +4,7 @@ namespace Zen4Dev;
 define ( 'FM_API_KEY', env('FM_API_KEY') );
 define ( 'FM_API_SECRET', env('FM_API_SECRET') );
 
-require_once __DIR__ .'/../REST-API/class.rest.php';
+require_once __DIR__ .'/../class.rest.php';
 
 class FreshMail extends \FmRestApi {
 
